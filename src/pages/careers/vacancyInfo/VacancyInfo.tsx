3663d16d39623150d832b6f1ac67c883693cbb70
dmitry.b <dmitry.b@zala.com.au>
2024-03-25 12:15:34 +0200

    fix titles' styles

--- src/pages/careers/vacancyInfo/VacancyInfo.tsx
+++ src/pages/careers/vacancyInfo/VacancyInfo.tsx
@@ -9,11 +9,11 @@ const VacancyInfo = ({ vacancy }: VacancyInfoProps) => {
   return (
     <a href="#">
       <div className="flex py-2 items-center justify-between sm:flex-col">
-        <div className="flex flex-col">
+        <div className="flex flex-col lg:text-[14px]">
           <p className="font-semibold">{vacancy.job.toUpperCase()}</p>
           <p>{vacancy.city.toUpperCase()}</p>
         </div>
-        <div className="flex max-w-[1000px] w-full justify-between sm:flex-col sm:mt-2">
+        <div className="flex max-w-[1000px] w-full justify-between sm:flex-col sm:mt-2 lg:text-[13px]">
           <p className="max-w-[450px] sm:text-start">{vacancy.requirements}</p>
           <p className="max-w-[400px] sm:text-start">{vacancy.responsibilities}</p>
         </div>
